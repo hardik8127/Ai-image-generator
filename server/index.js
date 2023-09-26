46733a30207e220d3a9e7793c5ab96405a3777f2
hardik8127 <hardikkumar159515@gmail.com>
2023-09-27 00:57:44 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,19 +1,33 @@
 import express from "express";
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit:'50mb'}));
+app.use(express.json({ limit: "50mb" }));
 
-app.get('/', async(req, res) => {
-    res.send('Hello form DALL-E');
-})
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+
+app.get("/", async (req, res) => {
+  res.send("Hello form DALL-E");
+});
 
 const startServer = async () => {
-    app.listen(8080, () => console.log('server has started on port http://localhost:8080'))
-}
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("server has started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 startServer();
